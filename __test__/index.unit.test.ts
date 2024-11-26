08d37744e92b33d77e1c7329e4d9efc313b2784a
Nick Berilov <n.berilov@mifort.org>
2024-11-26 13:19:49 +0100

    feat: implement raw pdf image and text extraction

--- __test__/index.unit.test.ts
+++ __test__/index.unit.test.ts
@@ -1,9 +1,133 @@
 import { describe, expect, it } from "vitest";
+import { extractTextAndImages } from "../index.js";
+import path from "path";
+import { fileURLToPath } from "url";
+import fs from "node:fs/promises";
+import { tmpdir } from "os";
+import { randomUUID, createHash } from "crypto";
+import { existsSync, mkdirSync, readFile, readFileSync } from "fs";
 
-import { sum } from "../index.js";
+describe("pdf export", async (t) => {
+  const __dirname = path.dirname(fileURLToPath(import.meta.url));
+  const pdfPath = path.join(__dirname, "./pdf-test-with-images.pdf");
 
-describe("pdf image export", (t) => {
-  it("sum from native", () => {
-    expect(sum(1, 2)).toBe(3);
+  const imagesFolderPath = path.join(tmpdir(), randomUUID());
+  if (!existsSync(imagesFolderPath)) {
+    mkdirSync(imagesFolderPath);
+  }
+  for (const file of await fs.readdir(imagesFolderPath)) {
+    await fs.unlink(path.join(imagesFolderPath, file));
+  }
+
+  it("should extract images and text from pdf", async () => {
+    const res = extractTextAndImages(pdfPath, imagesFolderPath);
+
+    // check text extraction
+    expect(res).toStrictEqual([
+      {
+        pageImages: [
+          {
+            filename: "image-1.png",
+            relatedText: ["1. How to program", "AA-FFF222 - AY"],
+          },
+        ],
+        pageTextLines: ["1. How to program", "AA-FFF222 - AY", "TSCode V1.2"],
+      },
+      {
+        pageImages: [],
+        pageTextLines: [
+          "List of required items",
+          "N. Index Item",
+          "1. A TSCode version 1.2",
+          "2. B PC or laptop",
+          "3. C Table",
+          "4. D Mouse or touchpad",
+          "5. I Good mood",
+          "6. F Internet",
+          "7. G Keyboard",
+          "8. H Browser",
+        ],
+      },
+      {
+        pageImages: [
+          {
+            filename: "image-2.png",
+            relatedText: [
+              "See animal below.",
+              "is a treatise on the theory of ethics, very popular during the Renaissance.",
+            ],
+          },
+        ],
+        pageTextLines: [
+          "What is Lorem Ipsum?",
+          "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+          "Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and",
+          "scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,",
+          "remaining essentially unchanged.",
+          "It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop",
+          "publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+          "Contrary to popular belief, is not simply random text. It has roots in a piece of classical",
+          "Latin literature from 45 BC, making it over 2000 years old.",
+          "Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked",
+          "up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and",
+          "going through the cites of the word in classical literature, discovered the undoubtable",
+          'source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum',
+          'et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book',
+          "is a treatise on the theory of ethics, very popular during the Renaissance.",
+          "See animal below.",
+        ],
+      },
+      {
+        pageImages: [
+          {
+            filename: "image-3.png",
+            relatedText: ["Probably this is electricity:", "What is it?"],
+          },
+          {
+            filename: "image-4.png",
+            relatedText: ["Here is a key", "Some text here and there"],
+          },
+        ],
+        pageTextLines: [
+          "What is it?",
+          "Probably this is electricity:",
+          "Some text here and there",
+          "Here is a key",
+        ],
+      },
+    ]);
+
+
+    // check image files extraction
+    const image1hash = await calculateHashForBuffer(
+      readFileSync(`${imagesFolderPath}/image-1.png`)
+    );
+    const image2hash = await calculateHashForBuffer(
+      readFileSync(`${imagesFolderPath}/image-2.png`)
+    );
+    const image3hash = await calculateHashForBuffer(
+      readFileSync(`${imagesFolderPath}/image-3.png`)
+    );
+    const image4hash = await calculateHashForBuffer(
+      readFileSync(`${imagesFolderPath}/image-4.png`)
+    );
+    expect(image1hash).toBe(
+      "4de1a60f162b504d6fe7b931c25ce182a50175345039a51ebf291952ca97a700"
+    );
+    expect(image2hash).toBe(
+      "8eca99e6200d12fb24f3f789f38917036427d5c5e61c98c40487de3bf2d2a0f8"
+    );
+    expect(image3hash).toBe(
+      "521ffc0509eedf7bcb30d227c10eab545abeb848493b63b9b9f1f695b1f69738"
+    );
+    expect(image4hash).toBe(
+      "d87d22dd0a9b22857633ffc8457438466c0573b9885f4386d75564abe6da81ee"
+    );
   });
 });
+
+async function calculateHashForBuffer(data: Buffer) {
+  const hash = createHash("sha256");
+  hash.update(data);
+  return hash.digest("hex");
+}
--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,236 @@
 
 #[macro_use]
 extern crate napi_derive;
+use image::ImageFormat;
+use itertools::{Itertools, Position};
+use pdfium_render::prelude::*;
+use std::cmp::Ordering;
+use std::fs::create_dir_all;
+use std::path::Path;
+use std::sync::OnceLock;
+use std::{env, os};
+
+static PDFIUM: OnceLock<Pdfium> = OnceLock::new();
+
+#[napi(object)]
+/// Extracted image metadata
+pub struct ExtractedImageMeta {
+  /// Image filename
+  pub filename: String,
+  /// Two closest to image text lines above or below
+  pub related_text: Vec<String>,
+}
+
+#[napi(object)]
+pub struct ExtractedPage {
+  /// Page images
+  pub page_images: Vec<ExtractedImageMeta>,
+  /// Page text lines
+  pub page_text_lines: Vec<String>,
+}
+
+// top y position and item
+enum TextLineOrImage {
+  TextLine(String),
+  /// image filename
+  Image(String),
+}
+
+// allowed vertical objects position difference to consider them same line
+static SAME_LINE_RANGE_DIFF: f32 = 5.0;
 
 #[napi]
-pub fn sum(a: i32, b: i32) -> i32 {
-  a + b
+/// Extract text from pdf files in lines and images with related text
+pub fn extract_text_and_images(pdf_path: String, images_folder_path: String) -> Vec<ExtractedPage> {
+  let pdfium_platform_library_path = if env::consts::OS == "macos" {
+    "./pdfium-mac-x64/lib"
+  } else {
+    "./pdfium-linux-x64/lib"
+  };
+
+  // Init library once
+  let pdfium = PDFIUM.get_or_init(|| {
+    let binary_path = Pdfium::pdfium_platform_library_name_at_path(pdfium_platform_library_path);
+    let bindings = Pdfium::bind_to_library(binary_path).unwrap();
+    // Bind library to pdfium binary
+    let pdfium: Pdfium = Pdfium::new(bindings);
+
+    pdfium
+  });
+
+  // Create images folder if not exist
+  let images_folder_path = Path::new(&images_folder_path);
+  create_dir_all(images_folder_path).unwrap();
+  let mut image_filename_idx = 1;
+
+  let document: PdfDocument<'_> = pdfium.load_pdf_from_file(&pdf_path, None).unwrap();
+
+  let mut result: Vec<ExtractedPage> = vec![];
+
+  for page in document.pages().iter() {
+    // Retrieving the text from a text object is done internally by loading the "text page"
+    // associated with the page the object is attached to, then asking that text page for the
+    // text related to the object. Therefore, when iterating over many text objects (as we
+    // are doing here), it is slightly faster to load the text page once rather than loading
+    // it and closing it every time we access an object:
+    let text_page = page.text().unwrap();
+
+    let mut texts_and_images = page
+      .objects()
+      .iter()
+      .filter(|o| match o.object_type() {
+        PdfPageObjectType::Image => true,
+        PdfPageObjectType::Text => {
+          if let Some(text) = o.as_text_object() {
+            let object_text = text_page.for_object(text);
+            return !object_text.trim().is_empty();
+          }
+          false
+        }
+        _ => false,
+      })
+      .collect::<Vec<_>>();
+
+    // Now sort by the vertical position of each bounds rectangle we collected.
+    // We sort in ascending numeric order, but because the PDF coordinate space
+    // starts with the vertical 0 at the page bottom
+    texts_and_images.sort_by(|a, b| a.bounds().unwrap().top.cmp(&b.bounds().unwrap().top));
+    texts_and_images.sort_by(|a, b| {
+      let a_bounds = a.bounds().unwrap();
+      let b_bounds = b.bounds().unwrap();
+
+      if a_bounds.top == b_bounds.top
+        || (a_bounds.top.value - b_bounds.top.value).abs() < SAME_LINE_RANGE_DIFF
+      {
+        return b_bounds.left.cmp(&a_bounds.left);
+      }
+
+      Ordering::Equal
+    });
+    texts_and_images.reverse();
+
+    // sorted page text lines and images
+    let mut page_text_lines_and_images: Vec<TextLineOrImage> = vec![];
+
+    // iterator helpers
+    let mut page_text_line: String = "".to_owned();
+    let mut last_top_pos: f32 = -1.0;
+
+    texts_and_images
+      .iter()
+      .with_position()
+      .for_each(|(position, o)| {
+        match o.object_type() {
+          // extract images with related text
+          PdfPageObjectType::Image => {
+            if let Some(image) = o.as_image_object() {
+              if let Ok(image) = image.get_raw_image() {
+                let image_filename = format!("image-{}.png", image_filename_idx);
+                image_filename_idx += 1;
+                let img_path = images_folder_path.join(&image_filename);
+
+                image.save_with_format(img_path, ImageFormat::Png).unwrap();
+
+                // push text line if present
+                if !page_text_line.is_empty() {
+                  page_text_lines_and_images
+                    .push(TextLineOrImage::TextLine(page_text_line.clone()));
+                  page_text_line = "".to_owned();
+                }
+
+                page_text_lines_and_images.push(TextLineOrImage::Image(image_filename.clone()));
+              }
+            }
+          }
+          // extract text in lines
+          PdfPageObjectType::Text => {
+            let t = o.as_text_object().unwrap();
+
+            let top_pos = t.bounds().unwrap().top.value;
+
+            if last_top_pos == -1.0 {
+              page_text_line.push_str(&t.text().trim());
+            }
+            // text is on the same line with small vertical position misalignment
+            else if top_pos > last_top_pos - SAME_LINE_RANGE_DIFF {
+              page_text_line.push_str(" ");
+              page_text_line.push_str(&t.text().trim());
+            } else {
+              if !page_text_line.is_empty() {
+                page_text_lines_and_images.push(TextLineOrImage::TextLine(page_text_line.clone()));
+                page_text_line = "".to_owned();
+              }
+
+              page_text_line.push_str(&t.text().trim());
+            }
+          }
+          _ => {}
+        };
+
+        if position == Position::Last {
+          // last text line of page
+          if !page_text_line.is_empty() {
+            page_text_lines_and_images.push(TextLineOrImage::TextLine(page_text_line.clone()));
+          }
+        }
+
+        last_top_pos = o.bounds().unwrap().top.value;
+      });
+
+    // map result
+    let mut page_text_lines: Vec<String> = vec![];
+    let mut page_images: Vec<ExtractedImageMeta> = vec![];
+    page_text_lines_and_images
+      .iter()
+      .enumerate()
+      .with_position()
+      .for_each(|(position, (idx, item))| match item {
+        TextLineOrImage::Image(filename) => {
+          let related_text: Vec<String>;
+
+          if position == Position::First {
+            let next_two_text_lines: Vec<String> = page_text_lines_and_images
+              .iter()
+              .skip(idx)
+              .filter_map(|item| match item {
+                TextLineOrImage::TextLine(v) => Some(v.clone()),
+                _ => None,
+              })
+              .take(2)
+              .collect();
+
+            related_text = next_two_text_lines;
+          } else {
+            let mut previous_two_text_lines: Vec<String> = page_text_lines_and_images
+              .iter()
+              .skip(idx - 2)
+              .filter_map(|item| match item {
+                TextLineOrImage::TextLine(v) => Some(v.clone()),
+                _ => None,
+              })
+              .take(2)
+              .collect();
+            previous_two_text_lines.reverse();
+
+            related_text = previous_two_text_lines;
+          }
+
+          let meta = ExtractedImageMeta {
+            filename: filename.clone(),
+            related_text,
+          };
+          page_images.push(meta)
+        }
+        TextLineOrImage::TextLine(text) => page_text_lines.push(text.clone()),
+      });
+
+    let page_result = ExtractedPage {
+      page_images,
+      page_text_lines,
+    };
+
+    result.push(page_result);
+  }
+
+  result
 }
